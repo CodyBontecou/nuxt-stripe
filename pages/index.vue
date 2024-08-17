5a546cf885c75c4a4c496fdf385b3ca01dad1856
Cody Bontecou <bontecouc@gmail.com>
2024-08-17 22:04:11 +0800

    pricing section

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,5 @@
-<script setup lang="ts">
-const { status, signIn, signOut } = useAuth()
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>You are currently {{ status }}.</div>
-  <div v-if="status === 'authenticated'">
-    <button @click="signOut()">Sign out</button>
-  </div>
-  <div v-else>
-    <button @click="signIn('github')">Sign in with GitHub</button>
-  </div>
+  <PricingSection />
 </template>
